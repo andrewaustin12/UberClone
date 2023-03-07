83c3bc108732e49c8a303872c2e36201102bae49
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-03-07 16:59:48 -0500

    Added ride type data model and UI fixes for ride request view.

--- UberClone/UberClone/Core/Trips/RideRequestView.swift
+++ UberClone/UberClone/Core/Trips/RideRequestView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct RideRequestView: View {
+    @State private var selectedRideType: RideType = .uberX
     var body: some View {
         VStack {
             Capsule()
@@ -72,23 +73,30 @@ struct RideRequestView: View {
             
             ScrollView(.horizontal) {
                 HStack(spacing: 12) {
-                    ForEach(0 ..< 3, id: \.self) {_ in
+                    ForEach(RideType.allCases) { type in
                         VStack(alignment: .leading) {
-                            Image("uber-x")
+                            Image(type.imageName)
                                 .resizable()
                                 .scaledToFit()
                             
-                            VStack(spacing: 4) {
-                                Text("Uber X")
+                            VStack(alignment: .leading, spacing: 4) {
+                                Text(type.description)
                                     .font(.system(size: 14, weight: .semibold))
                                 Text("$22.43")
                                     .font(.system(size: 14, weight: .semibold))
                             }
-                            .padding(8)
+                            .padding()
                         }
                         .frame(width: 112, height: 140)
-                        .background(Color(.systemGroupedBackground))
+                        .foregroundColor(type == selectedRideType ? .white : .black)
+                        .background(Color(type == selectedRideType ? .systemBlue : .systemGroupedBackground))
+                        .scaleEffect(type == selectedRideType ? 1.2 : 1.0)
                         .cornerRadius(10)
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                selectedRideType = type
+                            }
+                        }
                     }
                 }
             }
---
+++ UberClone/UberClone/Models/RideType.swift
@@ -0,0 +1,32 @@
+//
+//  RideType.swift
+//  UberClone
+//
+//  Created by andrew austin on 3/7/23.
+//
+
+import Foundation
+
+enum RideType: Int, CaseIterable, Identifiable {
+    case uberX
+    case black
+    case uberXL
+    
+    var id: Int { return rawValue}
+    
+    var description: String {
+        switch self {
+        case .uberX: return "UberX"
+        case .black: return "UberBlack"
+        case .uberXL: return "UberXL"
+        }
+    }
+    
+    var imageName: String {
+        switch self {
+        case .uberX: return "uber-x"
+        case .black: return "uber-black"
+        case .uberXL: return "uber-x"
+        }
+    }
+}
